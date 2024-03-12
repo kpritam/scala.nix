f192893cf0ffa7fd1923e5c322e922d9f4d10b90
Pritam Kadam <phkadam2008@gmail.com>
2024-03-12 12:20:54 +0530

    Add basic flake.nix

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Dev shell for Scala and Node";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            scala_2_13
+            jdk11
+            sbt
+            nodejs_20
+          ];
+        };
+      }
+    );
+}
+
